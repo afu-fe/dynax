4c311aaec09b9f6b17a205097723168d1b3ef4b0
hank <hank@AT9474.local>
2024-04-26 19:35:27 +0800

    feat: create-dyanx init

---
+++ packages/create-dynax/README.md
@@ -0,0 +1,14 @@
+# create-dynax <a href="https://npmjs.com/package/create-dynax"><img src="https://img.shields.io/npm/v/create-dynax" alt="npm package"></a>
+##  Scaffolding Your First Vite Project
+>  **Compatibility Note:**
+>  This scaffolding is only compatible with Vite 4.0 and above.
+
+With NPM:
+```bash
+  $ npm create dynax@latest
+```
+
+With PNPM:
+```bash
+  $ pnpm create dynax
+```
---
+++ packages/create-dynax/src/frameworks.ts
@@ -0,0 +1,80 @@
+import {
+  blue,
+  cyan,
+  green,
+  red,
+  reset,
+  yellow,
+} from 'kolorist'
+
+
+/**
+ *  framworks 框架数据
+ * 
+ * @returns [{}]
+ */
+const frameworks = [
+  {
+    name: 'vue',
+    display: 'Vue',
+    color: green,
+    variants: [
+      {
+        name: 'vue-ts',
+        display: 'TypeScript',
+        color: blue,
+      },
+      {
+        name: 'vue',
+        display: 'JavaScript',
+        color: yellow,
+      },
+      {
+        name: 'custom-create-vue',
+        display: 'Customize with create-vue ↗',
+        color: green,
+        customCommand: 'npm create vue@latest TARGET_DIR',
+      }
+    ],
+  },
+  {
+    name: 'react',
+    display: 'React',
+    color: cyan,
+    variants: [
+      {
+        name: 'react-ts',
+        display: 'TypeScript',
+        color: blue,
+      },
+      {
+        name: 'react-swc-ts',
+        display: 'TypeScript + SWC',
+        color: blue,
+      },
+      {
+        name: 'react',
+        display: 'JavaScript',
+        color: yellow,
+      },
+      {
+        name: 'react-swc',
+        display: 'JavaScript + SWC',
+        color: yellow,
+      }
+    ],
+  },
+  {
+    name: 'react native',
+    display: 'React Native',
+    color: red,
+    variants: [
+      {
+        name: 'react-native-ts',
+        display: 'TypeScript',
+        color: reset,
+      },
+    ]
+  }
+]
+export default frameworks
